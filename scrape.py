d91ebf0f95eab35e9e207417ca15181b8e856b56
Cameron Lau <cameron1998@gmail.com>
2021-01-10 22:58:59 -0800

    scrape job title/company/location

--- scrape.py
+++ scrape.py
@@ -3,9 +3,24 @@ from bs4 import BeautifulSoup
 
 # Customizable Inputs
 job = 'Software Engineer Intern'
-location = '95014'
+location = '94555'
 
 # Setup
 job = job.replace(' ','+')
 url = requests.get('https://www.indeed.com/jobs?q={}&l={}'.format(job, location))
 soup = bs4.BeautifulSoup(url.text, features="html.parser")
+
+# Parse
+results = soup.find(id='resultsCol')
+job_elems = results.find_all('div', class_="jobsearch-SerpJobCard unifiedRow row result")
+
+for job_elem in job_elems:
+    title = job_elem.find('h2', class_='title')
+    company = job_elem.find('span', class_='company')
+    location = job_elem.find('span', class_='location accessible-contrast-color-location')   
+    if None in (title, company, location):
+        continue
+    print(title.text.strip())
+    print(company.text.strip())
+    print(location.text.strip())
+    print("------")
