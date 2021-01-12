8eff7409afb041c5eb8c966f71d9c0658c5bad40
Cameron Lau <cameron1998@gmail.com>
2021-01-12 14:16:49 -0800

    check if apply link goes to company page or indeed

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
-import bs4, requests
+import bs4, requests, os, time
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # Customizable Inputs
 job = 'Software Engineer Intern'
@@ -10,6 +12,13 @@ job = job.replace(' ','+')
 url = 'https://www.indeed.com/jobs?q={}&l={}'.format(job, location)
 page = requests.get(url)
 soup = bs4.BeautifulSoup(page.text, features="html.parser")
+
+# Selenium Setup, uses chrome version 87
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+chrome_options = Options()
+chrome_options.add_argument("--headless")   # Browser stays closed
+browser = webdriver.Chrome(executable_path = DRIVER_BIN, chrome_options=chrome_options)
 
 # Parse
 results = soup.find(id='resultsCol')
@@ -26,10 +35,29 @@ for job_elem in job_elems:
     # Retrieve the links to each job
     address = job_elem.find('a')['href']
     combined_link = "https://www.indeed.com" + address
-    links.append(combined_link)
+    links.append(str(combined_link))
 
+    # Go to detailed job pages and scrape the job apply link
+    browser.get(combined_link)
+    time.sleep(1)   # give browser 1 second to load the page
+    detail_soup = bs4.BeautifulSoup(browser.page_source, features='html.parser')
+    job_path = detail_soup.find(id='viewJobButtonLinkContainer')
+
+    # Some jobs can be applied directly on indeed
+    apply_on_indeed = False
+    if job_path is None:
+        apply_on_indeed = True
+    else:
+        company_path = job_path.find('a')['href']
+
+    # Print data gathered
     print(title.text.strip())
     print(company.text.strip())
     print(location.text.strip())
-    print(combined_link)
+    if apply_on_indeed is True:
+        print("--- APPLY ON INDEED ---")
+        print(combined_link)
+    else:
+        print("--- COMPANY LINK --- ")
+        print(company_path)
     print("------")
