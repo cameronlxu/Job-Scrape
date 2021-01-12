bd48c63cc681c9f24d920f2d06b2ebb78268f8b4
Cameron Lau <cameron1998@gmail.com>
2021-01-12 10:57:03 -0800

    job details indeed link now stored

--- scrape.py
+++ scrape.py
@@ -7,12 +7,14 @@ location = '94555'
 
 # Setup
 job = job.replace(' ','+')
-url = requests.get('https://www.indeed.com/jobs?q={}&l={}'.format(job, location))
-soup = bs4.BeautifulSoup(url.text, features="html.parser")
+url = 'https://www.indeed.com/jobs?q={}&l={}'.format(job, location)
+page = requests.get(url)
+soup = bs4.BeautifulSoup(page.text, features="html.parser")
 
 # Parse
 results = soup.find(id='resultsCol')
 job_elems = results.find_all('div', class_="jobsearch-SerpJobCard unifiedRow row result")
+links = []
 
 for job_elem in job_elems:
     title = job_elem.find('h2', class_='title')
@@ -20,7 +22,14 @@ for job_elem in job_elems:
     location = job_elem.find('span', class_='location accessible-contrast-color-location')   
     if None in (title, company, location):
         continue
+
+    # Retrieve the links to each job
+    address = job_elem.find('a')['href']
+    combined_link = "https://www.indeed.com" + address
+    links.append(combined_link)
+
     print(title.text.strip())
     print(company.text.strip())
     print(location.text.strip())
+    print(combined_link)
     print("------")
