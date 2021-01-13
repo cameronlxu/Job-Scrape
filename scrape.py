823c55d598040c00fbbc2739743888a29aa2e0a8
Cameron Lau <cameron1998@gmail.com>
2021-01-12 20:50:30 -0800

    reformatted code, save all data to one list

--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-import bs4, requests, os, time
+import bs4, requests, os, time, csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,57 +7,61 @@ from selenium.webdriver.chrome.options import Options
 job = 'Software Engineer Intern'
 location = '94555'
 
-# Setup
-job = job.replace(' ','+')
-url = 'https://www.indeed.com/jobs?q={}&l={}'.format(job, location)
-page = requests.get(url)
-soup = bs4.BeautifulSoup(page.text, features="html.parser")
+def main():
+    # Setup
+    url = 'https://www.indeed.com/jobs?q={}&l={}'.format(job.replace(' ','+'), location)
+    page = requests.get(url)
+    soup = bs4.BeautifulSoup(page.text, features="html.parser")
 
-# Selenium Setup, uses chrome version 87
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
-chrome_options = Options()
-chrome_options.add_argument("--headless")   # Browser stays closed
-browser = webdriver.Chrome(executable_path = DRIVER_BIN, chrome_options=chrome_options)
+    # Selenium Setup, uses chrome version 87
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")   # Browser stays closed
+    browser = webdriver.Chrome(executable_path = DRIVER_BIN, options=chrome_options)
 
-# Parse
-results = soup.find(id='resultsCol')
-job_elems = results.find_all('div', class_="jobsearch-SerpJobCard unifiedRow row result")
-links = []
+    # Start parsing the home page
+    parseHome(browser, soup)
 
-for job_elem in job_elems:
-    title = job_elem.find('h2', class_='title')
-    company = job_elem.find('span', class_='company')
-    location = job_elem.find('span', class_='location accessible-contrast-color-location')   
-    if None in (title, company, location):
-        continue
+def parseHome(browser, soup):
+    # Zone in on results column
+    results = soup.find(id='resultsCol')
+    job_elems = results.find_all('div', class_="jobsearch-SerpJobCard unifiedRow row result")
+    
+    # Save all data to an list with format: [company, title, location, link]
+    data = []
 
-    # Retrieve the links to each job
-    address = job_elem.find('a')['href']
-    combined_link = "https://www.indeed.com" + address
-    links.append(str(combined_link))
+    for job_elem in job_elems:
+        title = job_elem.find('h2', class_='title')
+        company = job_elem.find('span', class_='company')
+        location = job_elem.find('span', class_='location accessible-contrast-color-location')   
+        if None in (title, company, location):
+            continue
 
-    # Go to detailed job pages and scrape the job apply link
-    browser.get(combined_link)
-    time.sleep(1)   # give browser 1 second to load the page
-    detail_soup = bs4.BeautifulSoup(browser.page_source, features='html.parser')
-    job_path = detail_soup.find(id='viewJobButtonLinkContainer')
+        # Retrieve the links to each job
+        address = job_elem.find('a')['href']
+        combined_link = "https://www.indeed.com" + address
 
-    # Some jobs can be applied directly on indeed
-    apply_on_indeed = False
-    if job_path is None:
-        apply_on_indeed = True
-    else:
-        company_path = job_path.find('a')['href']
+        # Go to detailed job pages and scrape the job apply link
+        browser.get(combined_link)
+        time.sleep(1)   # give browser 1 second to load the page
+        detail_soup = bs4.BeautifulSoup(browser.page_source, features='html.parser')
+        job_path = detail_soup.find(id='viewJobButtonLinkContainer')
 
-    # Print data gathered
-    print(title.text.strip())
-    print(company.text.strip())
-    print(location.text.strip())
-    if apply_on_indeed is True:
-        print("--- APPLY ON INDEED ---")
-        print(combined_link)
-    else:
-        print("--- COMPANY LINK --- ")
-        print(company_path)
-    print("------")
+        # Format each index's data
+        to_add = [company.text.strip(), title.text.strip(), location.text.strip()]
+
+        # Some jobs can be applied directly on indeed, determine this then save link
+        if job_path is None:
+            to_add.append(str(combined_link))
+        else:
+            company_path = job_path.find('a')['href']
+            to_add.append(company_path)
+
+        # Add to full data list
+        data.append(to_add)
+        print(to_add)
+
+
+if __name__ == "__main__":
+    main()
